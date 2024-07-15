43a4a784a771678e88867c2cfa14da53b75ead2a
Bhupesh <78196557+Bhupesh1x@users.noreply.github.com>
2024-07-15 18:28:33 +0530

    Created utility function for sending error message and sending email after registration and verifying email.

---
+++ backend/src/lib/utils.ts
@@ -0,0 +1,73 @@
+import jwt, { JwtPayload } from "jsonwebtoken";
+import nodemailer from "nodemailer";
+import { Response } from "express";
+
+export const errorMessage = (
+  res: Response,
+  message?: string,
+  code?: number
+) => {
+  const errMessage = message || "Internal server error";
+  const statusCode = code || 500;
+
+  return res.status(statusCode).json({ message });
+};
+
+export const sendEmail = async (
+  email: string,
+  subject: string,
+  text: string
+) => {
+  try {
+    const transport = nodemailer.createTransport({
+      host: process.env.MAIL_HOST,
+      service: process.env.MAIL_SERVICE,
+      port: 587,
+      secure: true,
+      auth: {
+        user: process.env.MAIL_USER_EMAIL,
+        pass: process.env.MAIL_USER_PASS,
+      },
+    });
+
+    await transport.sendMail({
+      from: process.env.MAIL_USER_EMAIL,
+      to: email,
+      subject,
+      text,
+    });
+  } catch (error) {
+    console.log("sendEmail-error", error);
+    throw new Error(`${error}`);
+  }
+};
+
+type VerifyEmailTokenParams = {
+  userId: string;
+  token: string;
+};
+
+export const verifyEmailToken = async ({
+  userId,
+  token,
+}: VerifyEmailTokenParams) => {
+  try {
+    const decoded = (await jwt.verify(
+      token,
+      process.env.JWT_VERIFY_SECRET!
+    )) as JwtPayload;
+
+    if (!decoded) {
+      return false;
+    }
+
+    if (decoded.userId !== userId) {
+      return false;
+    }
+
+    return true;
+  } catch (error) {
+    console.log("VerifyEmailToken-error", error);
+    return false;
+  }
+};
