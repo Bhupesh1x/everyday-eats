3cf944ca95875de59eede0e5c5f17617d553d46a
Bhupesh <78196557+Bhupesh1x@users.noreply.github.com>
2024-07-15 18:27:15 +0530

    Backend: Created new model for user.

---
+++ backend/src/models/user.model.ts
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+
+type UserType = {
+  _id: string;
+  name: string;
+  email: string;
+  password: string;
+  isVerified: boolean;
+};
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    select: false,
+  },
+  isVerified: {
+    type: Boolean,
+    required: true,
+    select: false,
+  },
+});
+
+export const User = mongoose.model<UserType>("User", userSchema);
