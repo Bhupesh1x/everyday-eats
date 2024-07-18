368e48048cf11511951283b6686c9df698d68ec2
Bhupesh <78196557+Bhupesh1x@users.noreply.github.com>
2024-07-18 17:19:12 +0530

    Backend: changes related to backend deployment.

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,6 @@
 import cors from "cors";
-import express from "express";
 import cookieParser from "cookie-parser";
+import express, { Request, Response } from "express";
 import "dotenv/config";
 
 import connectDb from "./lib/connectDb";
@@ -22,8 +22,12 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use("/health", (req: Request, res: Response) => {
+  res.json({ message: "Health OK!" });
+});
+
 app.use("/api/user", userRoutes);
 
 app.listen(PORT, () => {
-  console.log(`server is runnig on port:${PORT}`);
+  console.log(`server is running on port:${PORT}`);
 });
