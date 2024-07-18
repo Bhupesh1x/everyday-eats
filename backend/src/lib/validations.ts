a3878cfc7005e420c9da5971becd9ef9ce64becc
Bhupesh <78196557+Bhupesh1x@users.noreply.github.com>
2024-07-18 21:09:10 +0530

    Backend: Created validation for create restaurant api.

--- backend/src/lib/validations.ts
+++ backend/src/lib/validations.ts
@@ -1,4 +1,4 @@
-import { check, validationResult } from "express-validator";
+import { body, check, validationResult } from "express-validator";
 import { NextFunction, Request, Response } from "express";
 
 import { errorMessage } from "./utils";
@@ -60,5 +60,38 @@ export const updateUserValidations = [
   check("country", "Country should be atleast have 3 characters")
     .ltrim()
     .isLength({ min: 3 }),
+  handleValidationErrors,
+];
+
+export const createRestaurantValidations = [
+  body("name", "Restaurant name must have atleast 2 characters")
+    .ltrim()
+    .isLength({ min: 2 }),
+  body("city", "City must have atleast 3 characters")
+    .ltrim()
+    .isLength({ min: 3 }),
+  body("country", "Country must have atleast 3 characters")
+    .ltrim()
+    .isLength({ min: 3 }),
+  body("deliveryPrice", "Delivery price must have a positive number").isFloat({
+    min: 0,
+  }),
+  body(
+    "estimatedDeliveryTime",
+    "Delivery time price must have a positive number"
+  ).isInt({ min: 0 }),
+  body("cuisines")
+    .isArray()
+    .withMessage("Cuisines must be an array")
+    .not()
+    .isEmpty()
+    .withMessage("Cuisines array cannot be empty"),
+  body("menuItems").isArray().withMessage("Menu items must be a array"),
+  body("menuItems.*.name")
+    .notEmpty()
+    .withMessage("Menu items name is required"),
+  body("menuItems.*.price")
+    .notEmpty()
+    .withMessage("Menu items price is required"),
   handleValidationErrors,
 ];
