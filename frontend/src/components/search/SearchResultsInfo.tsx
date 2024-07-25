b071f2df57d4c26bfe2b6da0101fe96d523d80ca
Bhupesh <78196557+Bhupesh1x@users.noreply.github.com>
2024-07-25 20:50:44 +0530

    Update SearchResultsInfo.tsx

--- frontend/src/components/search/SearchResultsInfo.tsx
+++ frontend/src/components/search/SearchResultsInfo.tsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 type Props = {
   city: string;
@@ -6,17 +6,19 @@ type Props = {
 };
 
 export const SearchResultsInfo = ({ city, total }: Props) => {
+  const navigate = useNavigate();
+
   return (
     <div className="flex flex-col lg:items-center lg:gap-2 lg:flex-row">
       <h1 className="text-2xl font-bold tracking-tight">
         {total} Restaurants found in {city}
       </h1>
-      <Link
-        to="/#search"
-        className="text-blue-600 underline hover:text-primary transition"
+      <p
+        onClick={() => navigate(-1)}
+        className="text-blue-600 underline hover:text-primary transition cursor-pointer"
       >
         Change Location
-      </Link>
+      </p>
     </div>
   );
 };
